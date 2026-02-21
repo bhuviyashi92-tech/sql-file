46d80001c5dc076133015648197b6cbc8dac8de2
bhuviyashi92-tech <bhuviyashi92@gmail.com>
2026-02-21 12:29:44 +0530

    sql file
    if else commend changed

---
+++ if else.py
@@ -0,0 +1,33 @@
+'''
+a=int(input("enter a val"))
+b=int(input("enter b val"))
+if(a<b):
+ print("b is big")
+else:
+ print("a is big")
+
+
+ 
+
+a=int(input("enter a val"))
+b=int(input("enter b val"))
+if(a<b):
+  print("a is smaller then b") 
+else:
+  print("b is smaller then a")
+
+  
+n=int(input("enter any value"))
+if(n>0):
+    print(n,"positive")
+else:
+    print(n,"negative")
+
+    
+'''  
+n=int(input("enter any value"))
+if(n%2==0):
+    print(n,"is even")
+else:
+    print(n,"is odd")
+
